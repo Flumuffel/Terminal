de28e73a89e85927604ba4a7c092d55c9c1890f2
Flumuffel <40270803+Flumuffel@users.noreply.github.com>
2022-06-01 11:15:39 +0200

    Update whoami.js

--- app/commands/whoami.js
+++ app/commands/whoami.js
@@ -1,22 +1,20 @@
 import {TermColors} from "../constants.js";
 import {colorize} from "../utils.js";
 
-const LAST_UPDATE = '2022-05-26';
+const LAST_UPDATE = '01/06/2022';
 
 const whoami = {
   id: "whoami",
   args: 0,
   description: 'display effective developer info',
   async exec(term, _args) {
-    term.writeln(colorize(TermColors.Green, 'name: ') + 'brian');
-    term.writeln(colorize(TermColors.Green, 'host: ') + window.location.host);
-    term.writeln(colorize(TermColors.Green, 'current position: ') + 'software engineer');
-    term.writeln(colorize(TermColors.Green, 'company: ') + 'Heartbeat Medical Solutions < https://heartbeat-med.de >');
-    term.writeln(colorize(TermColors.Green, 'location: ') + 'Munich, Germany');
+    term.writeln(colorize(TermColors.Green, 'Name: ') + 'Flumuffel');
+    term.writeln(colorize(TermColors.Green, 'Host: ') + window.location.host);
+    term.writeln(colorize(TermColors.Green, 'Derzeitige Position: ') + 'Ausbildung Informatik Systemtechnik');
+    term.writeln(colorize(TermColors.Green, 'Standort: ') + 'Schweiz');
     term.writeln(colorize(TermColors.Green, 'fav languages: ') + '[golang, rust, typescript]');
-    term.writeln(colorize(TermColors.Green, 'hobbies: ') + '[photography, music, electronics]');
-    term.writeln(colorize(TermColors.Green, 'blog: ') + 'https://protiumx.dev/blog');
-    term.writeln(colorize(TermColors.Green, 'last update: ') + LAST_UPDATE);
+    term.writeln(colorize(TermColors.Green, 'Hobbies: ') + '[programmieren, streamen, spielen]');
+    term.writeln(colorize(TermColors.Green, 'Letztes update: ') + LAST_UPDATE);
   },
 };
 
